a82e7d12f0cfb12515f1f6ed68cb3be0f848aba7
emex4gman <emex4gman@gmail.com>
2021-04-27 23:42:46 -0400

    finance

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ A new Flutter project with some Custom Ui(s).
 This is a fun side project to showcase some cool designs converted to flutter code. Enjoy
 
 All the UI would be in the lib/Ui folder
+
+## Finance Dark theme UI Design
+### [ Design Link]([https://link](https://dribbble.com/shots/15293350-Finance-Dark-theme-UI-Design))
+<br>
+<code><img width='150px' src='./assets/screenshot/finance1.jpeg'> </code>
+<code><img width='150px' src='./assets/screenshot/finance2.png'> </code>
--- lib/entry_page.dart
+++ lib/entry_page.dart
@@ -4,20 +4,35 @@ import 'package:flutter_cool_ui/services/navigation_service.dart';
 import 'UI/Finance_Dark_theme_UI_Design/screens/finance_home.dart';
 
 class EntryPage extends StatelessWidget {
-  const EntryPage({Key? key}) : super(key: key);
-
+  EntryPage({Key? key}) : super(key: key);
+  final List<String> _images = ["assets/screenshot/finance1.jpeg"];
+  final List<Widget> _uis = [FinanceHome()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Container(
-          child: ElevatedButton(
-            child: Text(""),
-            onPressed: () {
-              NavigatorSerice.navigateToScreen(FinanceHome());
-              print("object");
-            },
+      body: SafeArea(
+        child: GridView.builder(
+          itemCount: _images.length,
+          padding: const EdgeInsets.all(15),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 0.5,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
           ),
-        )));
+          itemBuilder: (_, index) {
+            return GestureDetector(
+              onTap: () => NavigatorSerice.navigateToScreen(_uis[index]),
+              child: Container(
+                child: Image.asset(
+                  _images[index],
+                  fit: BoxFit.contain,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
